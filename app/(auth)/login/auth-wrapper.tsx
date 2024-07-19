f6449d5a45757121fce68c65596caff10a29b62e
AatirNadim <aatir.nadim@gmail.com>
2024-07-20 00:32:38 +0530

    update: updated the code-base

--- app/(auth)/login/auth-wrapper.tsx
+++ app/(auth)/login/auth-wrapper.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import UserAuthForm from '@/components/forms/user-auth-form';
-import UserLoginForm from '@/components/forms/user-login-form';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import Link from 'next/link';
-import React from 'react';
+import UserAuthForm from "@/components/forms/user-auth-form";
+import UserLoginForm from "@/components/forms/user-login-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
+import React from "react";
 
-const AuthWraper = () => {
+const AuthParent = () => {
   const [login, setLogin] = React.useState(true);
 
   return (
     <div className="flex h-full items-start p-4 pt-12 lg:p-8 lg:pt-24 xl:pt-36 ">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">{login ? 'Login to ' : 'Register'} your account</h1>
+          <h1 className="text-2xl font-semibold tracking-tight">{login ? "Login to " : "Register"} your account</h1>
           <p className="text-sm text-muted-foreground">Enter your credentials below to login.</p>
         </div>
         <Tabs defaultValue="user">
@@ -29,9 +29,9 @@ const AuthWraper = () => {
           <TabsContent value="vendor">{login ? <UserLoginForm /> : <UserAuthForm />}</TabsContent>
         </Tabs>
         <span className="flex justify-center gap-1 space-x-2 text-sm">
-          {login ? 'New user?' : 'Already have an account?'}
+          {login ? "New user?" : "Already have an account?"}
           <button onClick={() => setLogin((prev) => !prev)} className="text-primary underline">
-            {login ? 'Create an account' : 'Login'}
+            {login ? "Create an account" : "Login"}
           </button>
         </span>
         <p className="px-8 text-center text-sm text-muted-foreground">
@@ -50,4 +50,4 @@ const AuthWraper = () => {
   );
 };
 
-export default AuthWraper;
+export default AuthParent;
--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,5 @@
-// Protecting routes with next-auth
-// https://next-auth.js.org/configuration/nextjs#middleware
-// https://nextjs.org/docs/app/building-your-application/routing/middleware
+import { NextRequest, NextResponse } from "next/server";
 
-// import NextAuth from 'next-auth';
-// import authConfig from './auth.config';
-
-// const { auth } = NextAuth(authConfig);
-
-// export default auth((req) => {
-//   if (!req.auth) {
-//     const url = req.url.replace(req.nextUrl.pathname, '/');
-//     return Response.redirect(url);
-//   }
-// });
-
-// export const config = { matcher: ['/dashboard/:path*'] };
-
-// import { isUserAuthenticated } from '@/session';
-import { NextRequest, NextResponse } from 'next/server';
-
-// const protectedRoutes = [''];
 export default function middleware(req: NextRequest) {
-  // const user = localStorage.getItem('user');
-
-  // console.log(
-  //   '\n\nmiddleware: ',
-  //   req.nextUrl.pathname,
-  //   '\n\n user: ',
-  //   user,
-  //   '\n\n'
-  // );
-
-  // console.log('\n\nmiddleware: ', req.headers.get('authorization'), '\n\n');
-
-  // if (protectedRoutes.includes(req?.nextUrl?.pathname)) {
-  //   console.log('redirecting to / from middleware, /dashboard');
-
-  //   const absoluteUrl = new URL('/', req.nextUrl.origin);
-  //   return NextResponse.redirect(absoluteUrl.toString());
-  // }
   return NextResponse.next();
 }
