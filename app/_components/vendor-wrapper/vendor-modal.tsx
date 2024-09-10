f4b50a70895cf450fdb007815ffbff87a3f84c5d
m3rashid <m3rashid.hussain@gmail.com>
2024-09-10 15:25:43 +0530

    remove lockfile

--- app/_components/vendor-wrapper/vendor-modal.tsx
+++ app/_components/vendor-wrapper/vendor-modal.tsx
@@ -17,22 +17,20 @@ type VendorProps = {
 const VendorModal = ({ vendorType, description }: VendorProps) => {
   return (
     <motion.section
-      initial={{ opacity: 0, translateY: "5rem" }}
-      viewport={{
-        once: true
-      }}
-      whileInView={{ opacity: 1, translateY: 0 }}
+      viewport={{ once: true }}
       transition={{ duration: 0.5 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+      initial={{ opacity: 0, translateY: "5rem" }}
     >
       <Card className="flex w-[400px] flex-col items-center justify-center overflow-hidden ring-2 ring-primary-foreground ring-offset-4">
         <CardTitle className=" w-full overflow-hidden">
           <Image
-            src={vendorType.image || ""}
-            alt={vendorType.type}
             width={400}
             height={250}
             layout="responsive"
+            alt={vendorType.type}
             className="object-contain"
+            src={vendorType.image || ""}
           />
         </CardTitle>
         <CardHeader className="text-xl font-semibold">{vendorType.type}</CardHeader>
