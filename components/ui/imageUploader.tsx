80ada32a0c91d955ed033c00312af1c9dd3af40d
m3rashid <m3rashid.hussain@gmail.com>
2024-09-16 21:10:09 +0530

    slider slider images

--- components/ui/imageUploader.tsx
+++ components/ui/imageUploader.tsx
@@ -20,7 +20,7 @@ function ImageUploader(props: FormImageUploaderProps) {
       setLoading(true);
       const formData = new FormData();
       formData.append("file", file);
-      formData.append("upload_preset", "sacredshadi_unsigned");
+      formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || "");
 
       const res = await fetch(
         `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
