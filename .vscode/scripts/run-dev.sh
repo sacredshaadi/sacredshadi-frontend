947a4bc646eb8cae210895be16bbde494bcc9a6d
AatirNadim <aatir.nadim@gmail.com>
2024-10-19 19:22:41 +0530

    chonks

--- .vscode/scripts/run-dev.sh
+++ .vscode/scripts/run-dev.sh
@@ -4,6 +4,8 @@ if [ -f "yarn.lock" ]; then
   yarn dev
 elif [ -f "package-lock.json" ]; then
   npm run dev
+elif [ -f "pnpm-lock.yaml" ]; then
+  pnpm dev
 else
   echo "No recognized package manager found."
 fi
