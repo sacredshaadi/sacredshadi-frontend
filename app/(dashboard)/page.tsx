b3b230467f83849b09119b85e15cadcedf21bffd
m3rashid <m3rashid.hussain@gmail.com>
2024-10-21 19:08:59 +0530

    fix(home): add blogs on home page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -23,24 +23,30 @@ export default async function page() {
 
   return (
     <ScrollArea className="h-full">
-      <div className="flex flex-1 flex-col items-center space-y-4 ">
+      <div className="flex flex-1 flex-col items-center space-y-4">
         <CarouselComp sliderArr={slider || []} />
         <section className=" relative flex w-full flex-col items-center justify-center gap-8 overflow-visible p-4 sm:w-4/5 lg:w-3/5 lg:flex-row">
           <VendorSelectWrapper vendors={vendorTypes} cities={cities} />
         </section>
+
         <VendorWrapper vendorTypes={vendorTypes || []} />
       </div>
 
-      <h2 className="mb-8 mt-10 text-center text-3xl font-semibold tracking-tight text-primary drop-shadow-lg lg:text-4xl xl:text-5xl">
-        Blogs
-      </h2>
-      <p className="container prose text-center text-lg text-muted-foreground">
-        In this section we cover various sections ranging from the latest makeup ideas to the most fashionable outfits,
-        which is surely going to WOW you.
-      </p>
-      <section className="container my-4">
-        <BlogGrid userSide noPagination />
-      </section>
+      <div className="flex flex-1 flex-col items-center space-y-4">
+        <section className="container my-4 flex flex-col items-center justify-center gap-12 p-4">
+          <header className="flex w-full flex-col items-center justify-center gap-4 md:gap-8 lg:w-4/5 xl:w-3/5 xl:gap-12">
+            <h2 className="my-4 text-center text-3xl font-semibold tracking-tight text-primary drop-shadow-lg lg:text-4xl">
+              Blogs
+            </h2>
+            <p className="prose -mt-12 text-center text-lg text-muted-foreground">
+              In this section we cover various sections ranging from the latest makeup ideas to the most fashionable
+              outfits, which is surely going to WOW you.
+            </p>
+          </header>
+
+          <BlogGrid userSide noPagination />
+        </section>
+      </div>
     </ScrollArea>
   );
 }
